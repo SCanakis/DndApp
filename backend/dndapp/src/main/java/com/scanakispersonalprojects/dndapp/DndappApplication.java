de8ea0b5f0a962455ed79c52ccf0b07dd5c1ba2b
Sebastian Canakis <scanakis@Sebastians-MacBook-Pro-3.local>
2025-05-21 11:27:59 -0500

    backend enviorment created

---
+++ backend/dndapp/src/main/java/com/scanakispersonalprojects/dndapp/DndappApplication.java
@@ -0,0 +1,13 @@
+package com.scanakispersonalprojects.dndapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndappApplication.class, args);
+	}
+
+}
---
+++ backend/dndapp/src/test/java/com/scanakispersonalprojects/dndapp/DndappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scanakispersonalprojects.dndapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
